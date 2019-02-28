64a34b450370e64261416a59a1c8b0b014a0c6df
xce35g6 <tobias.bechtold@fiduciagad.de>
2019-02-28 13:01:19 +0100

    add dashboard component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,39 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatGridListModule,
+  MatIconModule, MatListModule,
+  MatMenuModule,
+  MatSidenavModule, MatToolbarModule
+} from '@angular/material';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        NoopAnimationsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+        MatGridListModule,
+        MatCardModule,
+        MatMenuModule,
+        LayoutModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        DashboardComponent
       ],
     }).compileComponents();
   }));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +21,7 @@ import { MatButtonModule, MatCheckboxModule, MatIconModule, MatListModule, MatSi
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatIconModule, MatListModule
+    MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
