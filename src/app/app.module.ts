73715bd513fee6e0dec806861c87b2b987587ad8
xce35g6 <tobias.bechtold@fiduciagad.de>
2019-02-28 13:06:46 +0100

    fix linting issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
+import {
+  MatButtonModule, MatCheckboxModule, MatIconModule, MatListModule,
+  MatSidenavModule, MatToolbarModule, MatGridListModule, MatCardModule, MatMenuModule
+} from '@angular/material';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LayoutModule } from '@angular/cdk/layout';
 
@@ -21,9 +24,15 @@ import { LayoutModule } from '@angular/cdk/layout';
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, LayoutModule
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
