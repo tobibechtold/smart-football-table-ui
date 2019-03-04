c0528b9e5ba144b1a7ec451a1e40787b6643068c
xce35g6 <tobias.bechtold@fiduciagad.de>
2019-03-04 19:01:36 +0100

    added heatmap integration

--- src/app/components/velocity/velocity.component.ts
+++ src/app/components/velocity/velocity.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { VelocityService } from '../../services/velocity.service';
+import { Velocity } from '../../models/velocity';
 
 @Component({
   selector: 'app-velocity',
@@ -7,12 +8,14 @@ import { VelocityService } from '../../services/velocity.service';
   styleUrls: ['./velocity.component.scss']
 })
 export class VelocityComponent implements OnInit {
-  _velocity = 0;
+  _velocity: Velocity = {velocity: 0};
 
   constructor(private velocityService: VelocityService) { }
 
   ngOnInit() {
-    this.velocityService.velocity().subscribe(velocity => this._velocity = JSON.parse(velocity.payload.toString()));
+    this.velocityService.velocity().subscribe(velocity =>
+      this._velocity = JSON.parse(velocity.payload.toString())
+    );
   }
 
 }
